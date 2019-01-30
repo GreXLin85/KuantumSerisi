15bd8fa706920d2580670cd474e34d2a83b8ce10
GreXLin85 <38141908+GreXLin85@users.noreply.github.com>
2019-01-30 21:44:35 +0300

    Update OrnekDriver1.cs

--- OrnekDriver1.cs
+++ OrnekDriver1.cs
@@ -8,12 +8,12 @@ namespace Quantum.QSharpApplication1
     {
         static void Main(string[] args)
         {
-            using (var qsim = new QuantumSimulator()) #Yeni kuantum simülasyonu oluştur
+            using (var qsim = new QuantumSimulator()) //Yeni kuantum simülasyonu oluştur
             {
-                HelloQ.Run(qsim).Wait(); #Simülasyon yani programı çalıştırmak için kullanılan komut
+                HelloQ.Run(qsim).Wait(); //Simülasyon yani programı çalıştırmak için kullanılan komut
             }
-            System.Console.Write("[DRIVER]Programdan çıkmak için herhangi bir tuşa basınız."); #Bu ve
-            Console.ReadKey(); #bu kısım simülasyon çalıştırılıp bittikten sonra direk kapatılmasını engellemesi sağlanmaktadır (Bu dosyada normal C# komutları kullanılınabilinir.)
+            System.Console.Write("[DRIVER]Programdan çıkmak için herhangi bir tuşa basınız."); //Bu ve
+            Console.ReadKey(); //bu kısım simülasyon çalıştırılıp bittikten sonra direk kapatılmasını engellemesi sağlanmaktadır (Bu dosyada normal C# komutları kullanılınabilinir.)
         }
     }
 }
